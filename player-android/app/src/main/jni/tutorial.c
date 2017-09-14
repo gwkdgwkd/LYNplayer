b474fdfc1848185e5d3ecbe488a472f392eade16
mlk <meilikelyn@126.com>
2017-09-14 22:29:06 +0800

    play the audio properly
    Signed-off-by: mlk <meilikelyn@126.com>

--- player-android/app/src/main/jni/tutorial.c
+++ player-android/app/src/main/jni/tutorial.c
@@ -19,6 +19,7 @@
 #include <libavformat/avformat.h>
 #include <libswscale/swscale.h>
 #include <libavutil/pixfmt.h>
+#include <libswresample/swresample.h>
 #include "libyuv.h"
 
 #include <stdio.h>
@@ -461,7 +462,8 @@ static int audio_decode_frame(AVCodecContext *codecCtx, uint8_t *audio_buf, int 
     static AVFrame frame;
 
     int len1, data_size = 0;
-
+    struct SwrContext *swr_ctx = NULL;
+    int resampled_data_size;
 
     for(;;) {
         while(audio_pkt_size > 0) {
@@ -484,14 +486,57 @@ static int audio_decode_frame(AVCodecContext *codecCtx, uint8_t *audio_buf, int 
                         codecCtx->sample_fmt,
                         1
                     );
-                memcpy(audio_buf, frame.data[0], data_size);
+                if (frame.channels > 0 && frame.channel_layout == 0){
+                    frame.channel_layout = av_get_default_channel_layout(frame.channels);
+                } else if (frame.channels == 0 && frame.channel_layout > 0) {
+                    frame.channels = av_get_channel_layout_nb_channels(frame.channel_layout);
+                }
+                /*
+                LOGI("frame.sample_rate = %d \n", frame.sample_rate);
+                LOGI("frame.format = %d \n", frame.format);
+                LOGI("frame.format bits = %d \n", av_get_bytes_per_sample(frame.format));
+                LOGI("frame.channels = %d \n", frame.channels);
+                LOGI("frame.channel_layout = %d \n", frame.channel_layout);
+                LOGI("frame.nb_samples = %d \n", frame.nb_samples);
+                */
+                if(frame.format != AV_SAMPLE_FMT_S16
+                    || frame.channel_layout != codecCtx->channel_layout
+                    || frame.sample_rate != codecCtx->sample_rate
+                    || frame.nb_samples != 1024) {
+                    if (swr_ctx != NULL) {
+                        swr_free(&swr_ctx);
+                        swr_ctx = NULL;
+                    }
+                    swr_ctx = swr_alloc_set_opts(NULL, frame.channel_layout,
+                        AV_SAMPLE_FMT_S16, frame.sample_rate,frame.channel_layout,
+                        (enum AVSampleFormat)frame.format, frame.sample_rate, 0, NULL);
+                    if (swr_ctx == NULL || swr_init(swr_ctx) < 0) {
+                        LOGE("swr_init failed!!!" );
+                        break;
+                    }
+                }
+                if(swr_ctx) {
+                    int dst_nb_samples = av_rescale_rnd(swr_get_delay(swr_ctx, frame.sample_rate) + frame.nb_samples,
+                                                        frame.sample_rate, AV_SAMPLE_FMT_S16, AV_ROUND_INF);
+                    LOGI("swr convert ! \n");
+                    LOGI("dst_nb_samples : %d \n", dst_nb_samples);
+
+                    int len2 = swr_convert(swr_ctx, &audio_buf, dst_nb_samples,(const uint8_t**)frame.data, frame.nb_samples);
+                    if (len2 < 0) {
+                        LOGE("swr_convert failed \n" );
+                        break;
+                    }
+                    resampled_data_size = frame.channels * len2 * av_get_bytes_per_sample(AV_SAMPLE_FMT_S16);
+                }else{
+                    resampled_data_size = data_size;
+                }
             }
             if(data_size <= 0) {
                 /* No data yet, get more frames */
                 continue;
             }
             /* We have data, return it and come back for more later */
-            return data_size;
+            return resampled_data_size;
         }
         if(pkt.data) {
             av_free_packet(&pkt);
